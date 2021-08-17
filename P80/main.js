e90deef4e9d57a229fe9ece44bd24fa1fc6d6532
aharsi1298 <82213601+aharsi1298@users.noreply.github.com>
2021-08-17 20:24:57 +0530

    Add files via upload

---
+++ P80/main.js
@@ -0,0 +1,59 @@
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	// use the push function as - 
+        names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+    document.getElementById("name1").value = "";
+   }
+
+
+
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	// display the name in the id= sort_button
+	}
+
+
+function sorting()
+	{
+		names_of_people           // add the sort function
+		var nop = [];
+		for(const item in names_of_people) nop.push(names_of_people[item]);
+		nop.sort();
+		// .the sort function is .sort();
+		var i= nop.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
+
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
+}
+
+function save() {
+  //document.cookie = `guestlist=${names_of_people}`;
+  alert('This feature is not ready yet')
+}
