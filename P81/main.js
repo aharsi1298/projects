61d6473a4ca0030be9738b531fc7872c70db0da1
aharsi1298 <82213601+aharsi1298@users.noreply.github.com>
2021-08-19 17:16:11 +0530

    Add files via upload

---
+++ P81/main.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.arc(300, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.arc(275, 250, 40, 0, 2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.arc(325, 250, 40, 0, 2*Math.PI);
+ctx.stroke();
